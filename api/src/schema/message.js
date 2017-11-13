3017ee7f1a446868ac7a225a20845d748f35201b
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-11-13 23:50:23 +0700

    Adjusted some details

--- api/src/schema/message.js
+++ api/src/schema/message.js
@@ -2,13 +2,13 @@ export default `
 
     type Message {
         body: String!
-        sender: User!
-        reciever: User!
-        chat_room: String!
+        sender: String! 
+        reciever: String 
+        chat_room: String
     }
 
     type Subscription {
-        newMessageAdded(body: String!, reciever: String!, chat_room: String): Message!
+        newMessageAdded(chat_room: String): Message!
     }
 
     type Query {
