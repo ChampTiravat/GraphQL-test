23c1d2e62ff6aadf1d07eebee82a3c9ede72bfe1
Tiravat Thaisubvorakul <tiravat2016@gmail.com>
2017-11-13 23:51:16 +0700

    created a subscription and listen to the socket when new chat message is created

--- api/src/resolver/message.js
+++ api/src/resolver/message.js
@@ -1,7 +1,11 @@
-import { PubSub } from "graphql-subscriptions";
+import { PubSub, withFilter } from "graphql-subscriptions";
 
 import Message from "../models/Message";
 import { requiredAuth } from "../helpers/permission";
+
+const pubsub = new PubSub();
+
+const NEW_MESSAGE_ADDED_SUBSCRIPTION = "newMessageAdded";
 
 export default {
   Query: {
@@ -16,12 +20,25 @@ export default {
   Mutation: {
     createMessage: async (parent, { body }, { user }, info) => {
       try {
+        // Creating new messsage
         await Message.create({
           body,
           sender: "tiravat",
           reciever: "",
           chat_room: ""
         });
+
+        // Broadcasting this event. So, everyone can listen to this
+        pubsub.publish(NEW_MESSAGE_ADDED_SUBSCRIPTION, {
+          newMessageAdded: {
+            body,
+            sender: "tiravat",
+            reciever: "someone",
+            chat_room: "123"
+          }
+        });
+
+        // Send back an operation's status as GraphQL Type Boolean
         return true;
       } catch (err) {
         console.log(err);
@@ -29,5 +46,12 @@ export default {
       }
     }
   },
-  Subscription: {}
+  Subscription: {
+    newMessageAdded: {
+      subscribe: withFilter(
+        () => pubsub.asyncIterator(NEW_MESSAGE_ADDED_SUBSCRIPTION),
+        (payload, args) => true
+      )
+    }
+  }
 };
